11882a55c272811b094dacf96bae1cdd64f1291f
Yaniv Efraim <yaniv.efraim@gmail.com>
2017-08-31 22:42:01 +0300

    move to puppeteer

--- test/browser/app.e2e.spec.js
+++ test/browser/app.e2e.spec.js
@@ -1,23 +1,26 @@
-import {AppDriver} from './drivers/app.driver';
 import {expect} from 'chai';
+import 'babel-polyfill';
 import {beforeAndAfter} from '../environment';
-
-let appDriver;
+import puppeteer from 'puppeteer';
 
 describe('React application', () => {
+  let browser, page;
   beforeAndAfter();
 
-  beforeEach(async () => {
-    appDriver = new AppDriver();
+  before(async () => {
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
   });
 
-  afterEach(() => {
-    appDriver.close();
+  after(() => {
+    browser.close();
   });
 
-  it('should display two buttons', async () => {
-    await appDriver.navigate();
-    const title = await appDriver.getTitle();
-    expect(await title.getText()).to.eq('Hello World!');
+  describe('open page', () => {
+    it('should display title', async () => {
+      await page.goto('http://localhost:3100/');
+      const title = await page.$('h2');
+      expect(await title.evaluate(elem => elem.innerText)).to.eql('Hello World!');
+    });
   });
 });
--- test/environment.js
+++ test/environment.js
@@ -1,5 +1,6 @@
 import server from '../src/server';
 import {spawn} from 'child_process';
+import {sleep} from 'sleep';
 
 const app = server();
 export const beforeAndAfter = (port = 3100) => {
@@ -10,11 +11,14 @@ export const beforeAndAfter = (port = 3100) => {
 
   after(() => expressServer.close());
 
-  before(() => {
-    cdnServer = spawn('node', ['./dist/test/browser/mock/cdn-server.js'], {stdio: 'inherit'});
-  });
+  if (process.env.WIX_NODE_BUILD_WATCH_MODE !== 'true') {
+    before(() => {
+      cdnServer = spawn('node', ['./dist/test/browser/mock/cdn-server.js'], {stdio: 'inherit'});
+      sleep(1);
+    });
 
-  after(() => {
-    cdnServer.kill();
-  });
+    after(() => {
+      cdnServer.kill();
+    });
+  }
 };
